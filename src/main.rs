43585e677d94d9502183ceb6eef7d1e1e02885b2
Matthew Perry <matthew.perry@arcticshores.com>
2019-11-20 18:43:16 +0000

    Added login call to Slack

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use scrumbot::api_token;
+
+use slack::RtmClient;
+
+
 fn main() {
-    println!("Hello, world!");
+    let api_token = api_token::slack_api_token();
+    let client = RtmClient::login(&api_token);
+    
+    match client {
+        Ok(_) => {},
+        Err(err) => panic!("{}", err)
+    }
 }
