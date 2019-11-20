196079b358e413bce3a62081da50d3fda61d719c
Matthew Perry <matthew.perry@arcticshores.com>
2019-11-20 18:37:01 +0000

    Add tests for api_token error return

--- src/api_token.rs
+++ src/api_token.rs
@@ -4,6 +4,6 @@ use super::SLACK_API_TOKEN;
 pub fn slack_api_token() -> String {
     match env::var(SLACK_API_TOKEN){
         Ok(val) => String::from(val),
-        Err(e) => String::from(format!("{}", e)),
+        Err(_) => String::from("No api token provided"),
     }
 }
--- tests/setup_tests.rs
+++ tests/setup_tests.rs
@@ -4,6 +4,12 @@ use std::env;
 
 #[test]
 fn test_api_token_returns_env_if_set() {
-    env::set_var("SLACK_API_TOKEN", scrumbot::SLACK_API_TOKEN);
-    assert_eq!(scrumbot::SLACK_API_TOKEN, &scrumbot::api_token::slack_api_token());
+    env::set_var(scrumbot::SLACK_API_TOKEN, "TEST_STRING");
+    assert_eq!("TEST_STRING", &scrumbot::api_token::slack_api_token());
+}
+
+#[test]
+fn test_api_token_returns_error_message_if_unset() {
+    env::remove_var(scrumbot::SLACK_API_TOKEN);
+    assert_eq!("No api token provided", &scrumbot::api_token::slack_api_token());
 }
